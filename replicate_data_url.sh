1ffdec16d329cf04f5eb2d1249df76b59766f482
K3NW48 <noThanks>
2023-12-09 16:06:33 -0500

    add .env .gitignore

--- replicate_data_url.sh
+++ replicate_data_url.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+source .env
+
 RESPONSE=$(curl -s -X POST -H "Authorization: Token $REPLICATE_API_TOKEN" https://dreambooth-api-experimental.replicate.com/v1/upload/data.zip)
 
 
